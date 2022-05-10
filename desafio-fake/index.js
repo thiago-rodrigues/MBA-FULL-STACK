f5135d808ab98b6c5d9dd878243e4506d69fd514
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2022-05-10 15:57:18 -0300

    Atualização dos exercicios

--- desafio-fake/index.js
+++ desafio-fake/index.js
@@ -3,12 +3,14 @@ let pedidos = JSON.stringify(Jsonfile);
 let teste = [];
 teste = JSON.parse(pedidos);
 
+/*
 for (let i = 0; i < teste.pedidos.length; i++) {
   if (teste.pedidos[i].id === 216) {
     console.log(teste.pedidos[i]);
   } else {
   }
 }
+*/
 
 let total = 0;
 for (let i = 0; i < teste.pedidos.length; i++) {
@@ -22,7 +24,7 @@ for (let i = 0; i < teste.pedidos.length; i++) {
   } else {
   }
    */
-
+  /*
   if (
     teste.pedidos[i].produto === "Pizza Atum" &&
     teste.pedidos[i].entregue === true
@@ -30,18 +32,37 @@ for (let i = 0; i < teste.pedidos.length; i++) {
     total = total + teste.pedidos[i].valor;
   } else {
   }
+  */
+  console.log("");
 }
 console.log(total);
 
-/*
-for (let i = 0; i < teste.length; i++) {
-  if (teste[i].brand === "Hummer") {
-    console.log(teste[i].models);
+let PizzaAtum = 0;
+let PizzaModa = 0;
+let PizzaCalabresa = 0;
+let PizzaNapolitana = 0;
+let PizzaMuçarela = 0;
+let PizzaFrango = 0;
+
+for (let i = 0; i < teste.pedidos.length; i++) {
+  if (teste.pedidos[i].produto === "Pizza Atum") {
+    PizzaAtum += 1;
+  } else if (teste.pedidos[i].produto === "Pizza a Moda") {
+    PizzaModa += 1;
+  } else if (teste.pedidos[i].produto === "Pizza Calabresa") {
+    PizzaCalabresa += 1;
+  } else if (teste.pedidos[i].produto === "Pizza Napolitana") {
+    PizzaNapolitana += 1;
+  } else if (teste.pedidos[i].produto === "Pizza Muçarela") {
+    PizzaMuçarela += 1;
+  } else if (teste.pedidos[i].produto === "Pizza Frango com Catupiry") {
+    PizzaFrango += 1;
   }
 }
-*/
-/*
-for (let i = 0; i < teste.length; i++) {
-  console.log(teste[i].brand + ":" + teste[i].models.length);
-}
-*/
+
+console.log("Pizza Atum: " + PizzaAtum);
+console.log("Pizza a Moda: " + PizzaModa);
+console.log("Pizza Calabresa: " + PizzaCalabresa);
+console.log("Pizza Napolitana: " + PizzaNapolitana);
+console.log("Pizza Muçarela: " + PizzaMuçarela);
+console.log("Pizza Frango Catupiry: " + PizzaFrango);
--- rotas/index.js
+++ rotas/index.js
@@ -1,5 +1,7 @@
 import express, { response } from "express";
+
 const app = express();
+app.use(express.json());
 
 app.all("/testeall", (req, res) => {
   res.send(req.method);
@@ -24,6 +26,59 @@ app.post("/test(ing)?", (req, resp) => {
   res.send(req.path);
 });
 
+/*Parametros na rota e parametro opcional*/
+app.get("/testparam/:id/:id2?", (req, res) => {
+  res.send(req.params.id + " - " + req.params.id2);
+});
+
+/*Expressão regular */
+app.get(/.*red$/, (req, res) => {
+  res.send("/.*red$/");
+});
+
+app.post("/testbody", (req, resp) => {
+  console.log(req.body);
+  resp.send(req.path);
+});
+
+/*parametros via query */
+app.get("/testQuery", (req, resp) => {
+  resp.send(req.query);
+});
+
+/*next */
+app.get(
+  "/testmultiple",
+  (req, resp, next) => {
+    console.log("Callback 1");
+    next();
+  },
+  (req, res) => {
+    console.log("Callback 2");
+    res.send("Fim!"); /*resposta para finalizar */
+    res.end(); /* Ou end */
+  }
+);
+
+/*Next com Array */
+const callback1 = (req, resp, next) => {
+  console.log("Callback 1");
+  next();
+};
+
+function callback2(req, res, next) {
+  console.log("Callback 2");
+  next();
+}
+
+const callback3 = (req, resp) => {
+  console.log("Callback 3");
+  resp.end();
+};
+
+app.get("/testmultiplearray", [callback1, callback2, callback3]);
+
+/*Start na porta definida */
 app.listen(3000, () => {
   console.log("Server Started!");
 });
